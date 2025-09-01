5a7bb51fc79c94e914ed7c12542c4c142741ca09
irvinganalysis27 <irvinganalysis@gmail.com>
2025-09-01 21:54:59 +0100

    Update app.py

--- app.py
+++ app.py
@@ -60,9 +60,10 @@ position_metrics = {
             "Offensive duels per 90": "Possession",
             "Offensive duels won, %": "Possession",
             "Passes to final third per 90": "Possession",
-            "Accurate passes to final third, %": "Possession"
-            "xA per 90": "Attacking"
+            "Accurate passes to final third, %": "Possession",
+            "xA per 90": "Attacking",
             "Assists per 90": "Attacking"
+}
 
         }
     },
